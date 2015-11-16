be6165121c3a4744c966763f9e5d24a941685c84
lchabbani <lalunachabbani@Las-MacBook-Air.local>
2015-11-15 22:04:23 -0800

    update

--- p3/Website/website.js
+++ p3/Website/website.js
@@ -1,66 +1,19 @@
-var letters = $('.letters').html().split('');
-var letterArray = [];
+$('.letters').each(function() {
 
-for (i = 0; i < letters.length; i++) {
+  var $letterWrapper = $(this);
+  var letters = $letterWrapper.html().split('');
+  var letterArray = [];
+
+  for (i = 0; i < letters.length; i++) {
     console.log(letters[i]);
- 
-	var character = letters[i];
-    var chracterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
-	var characterHTML = '<div class="letter" data-character="' + character + '">' + chracterDefinition + '</div>';
-    
-    letterArray.push(characterHTML);
-};
 
-$('.letters').html(letterArray);
-
-
-var letters2 = $('.letters2').html().split('');
-var letterArray = [];
-
-for (i = 0; i < letters2.length; i++) {
-    console.log(letters2[i]);
- 
-	var character = letters2[i];
-    var chracterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
-	var characterHTML = '<div class="letter" data-character="' + character + '">' + chracterDefinition + '</div>';
-    
-    letterArray.push(characterHTML);
-};
-
-$('.letters2').html(letterArray);
-
-
-var letters3 = $('.letters3').html().split('');
-var letterArray = [];
-
-for (i = 0; i < letters3.length; i++) {
-    console.log(letters3[i]);
- 
-	var character = letters3[i];
-    var chracterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
-	var characterHTML = '<div class="letter" data-character="' + character + '">' + chracterDefinition + '</div>';
-    
-    letterArray.push(characterHTML);
-};
-
-$('.letters3').html(letterArray);
-
-
-var letters4 = $('.letters4').html().split('');
-var letterArray = [];
-
-for (i = 0; i < letters4.length; i++) {
-    console.log(letters4[i]);
- 
-    var character = letters4[i];
+    var character = letters[i];
     var chracterDefinition = $('.letter-definition[data-character="' + character + '"]').html();
     var characterHTML = '<div class="letter" data-character="' + character + '">' + chracterDefinition + '</div>';
     
     letterArray.push(characterHTML);
-};
+  };
 
-$('.letters4').html(letterArray);
+  $letterWrapper.html(letterArray);
 
-
-
-
+});
